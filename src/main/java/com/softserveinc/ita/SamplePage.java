4aef9a475ad2812971c3f4a059e751616829ebf3
Nikodych <shkliar.dmytro@gmail.com>
2021-10-07 22:30:07 +0300

    Initial commit

---
+++ src/main/java/com/softserveinc/ita/SamplePage.java
@@ -0,0 +1,5 @@
+package com.softserveinc.ita;
+
+public class SamplePage {
+
+}
---
+++ src/test/java/com/softserveinc/ita/SampleTest.java
@@ -0,0 +1,12 @@
+package com.softserveinc.ita;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTestMethod() {
+        Assert.assertTrue("true".equals("true"));
+    }
+}
