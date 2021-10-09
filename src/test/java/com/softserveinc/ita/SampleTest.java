effb36b3e62d333b1f49f81a2bba277402d167a9
vsaroz <vitaliyfrost1999@gmail.com>
2021-10-10 01:17:26 +0300

    Search by XPath and some selectors

--- src/test/java/com/softserveinc/ita/SampleTest.java
+++ src/test/java/com/softserveinc/ita/SampleTest.java
@@ -1,12 +1,23 @@
-package com.softserveinc.ita;
+package com.softserveinc.ita.vsaroz;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import java.util.concurrent.TimeUnit;
 
 public class SampleTest {
-
-    @Test
-    public void sampleTestMethod() {
-        Assert.assertTrue("true".equals("true"));
+    @BeforeSuite
+    public void beforeSuite() {
+        //Installing chrome driver
+        System.setProperty("webdriver.chrome.driver", "E://SoftServe//chromedriver_win32//chromedriver.exe");
     }
+    @Test
+    public void firstTest() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://mkt.uz.ua/");
+    }
+
 }
