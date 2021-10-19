f562ce98fe293dc79ee52c01dc8c76bee808fd89
Dmytro Krutenko <dospecwork@gmail.com>
2021-10-19 12:57:44 +0300

    Create rozetkaTests.java

---
+++ src/test/java/com/softserveinc/ita/dkrutenko/rozetkaTests.java
@@ -0,0 +1,72 @@
+package com.softserveinc.ita.dkrutenko;
+
+import com.softserveinc.ita.pageobjects.CartMenu;
+import com.softserveinc.ita.pageobjects.HomePage;
+import com.softserveinc.ita.pageobjects.SearchField;
+import com.softserveinc.ita.utils.runners.TestRunner;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class rozetkaTests extends TestRunner {
+
+    @DataProvider
+    public Object[][] rozetkaItems () {
+        return new Object[][]{
+                {"samsung",  "Samsung Galaxy A72"},
+                {"iphone",   "iPhone 12 Pro Max"},
+                {"samsung",  "Samsung Galaxy Watch 4"},
+                {"nokia",    "Nokia G10" }  };
+    }
+    @Test(dataProvider = "rozetkaItems")
+    //searchItem = samsung or etc; item = some phone or etc.
+    public void rozetkaSearchTest(String searchItem, String item) {
+        SearchField searchField = loadSearch();
+                  searchField.fillSearch(searchItem);
+                  searchField.clickSearchButton();
+                  searchField.waitElementCondition();
+       List<String> list = searchField.getGoodsList()
+               .stream()
+               .map(WebElement::getText)
+               .filter(text -> text.contains(item))
+               .collect(Collectors.toList());
+        Assert.assertTrue(list.get(0).contains(item));
+        HomePage homePage = loadHomePage();
+        homePage.clickHomePage();
+    }
+
+    @DataProvider
+    public Object[][] rozetkaCheckCartItems () {
+        return new Object[][]{
+                {"samsung", "Samsung Galaxy S21"},
+              //  {"iphone",   "iPhone 12 Pro Max"},
+            //    {"samsung", "Samsung Galaxy A72"}
+                    };
+    }
+    @Test(dataProvider = "rozetkaCheckCartItems")
+    //searchItem = samsung or etc; item = some phone or etc.
+    public void rozetkaCartFunctionalTest (String searchItem, String item) {
+        SearchField searchField = loadSearch();
+            searchField.fillSearch(searchItem);
+            searchField.clickSearchButton();
+            searchField.waitElementCondition();
+            searchField.getExpectedItem(item).click();
+        CartMenu cartMenu = loadCartMenu();
+        cartMenu.clickAddToCartButton();
+        cartMenu.waitElementCondition();
+        cartMenu.removeFromCart();
+
+
+        /* cartMenu.waitElementCondition();
+                cartMenu.clickHomePage();
+                cartMenu.clickCart();
+                    cartMenu.removeFromCart();
+                    cartMenu.clickCartClose();
+
+                */
+    }
+}
+
