975b6686832b38e55e22db3f3de2a533edeb2abe
Pavlo Kuravskyi <pkuravsky@gmail.com>
2021-10-26 20:23:47 +0300

    Fix

---
+++ src/main/java/com/softserveinc/ita/pkuravskyi/pageobjects/BasePage.java
@@ -0,0 +1,35 @@
+package com.softserveinc.ita.pkuravskyi.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage<T extends BasePage> {
+
+    protected WebDriver driver;
+    protected By searchBar;
+    protected By searchButton;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public T searchBarInputText(String searchText) {
+        driver
+                .findElement(searchBar)
+                .sendKeys(searchText);
+
+        return (T) this;
+    }
+
+    public T searchButtonClick() {
+        driver
+                .findElement(searchButton)
+                .click();
+
+        return (T) this;
+    }
+
+    public String getSearchBarText() {
+        return driver.findElement(searchBar).getAttribute("value");
+    }
+}
