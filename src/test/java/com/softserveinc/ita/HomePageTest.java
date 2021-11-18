667806710ebb3c4ee51ca7093cd8a8286b144c59
Pavlo Kuravskyi <pkuravsky@gmail.com>
2021-11-18 19:38:25 +0200

    Updated language switching test

--- src/test/java/com/softserveinc/ita/HomePageTest.java
+++ src/test/java/com/softserveinc/ita/HomePageTest.java
@@ -3,17 +3,20 @@ package com.softserveinc.ita;
 import com.softserveinc.ita.utils.runners.TestRunner;
 import org.testng.annotations.Test;
 
+import static com.softserveinc.ita.models.LanguageSwitcher.RU;
 import static com.softserveinc.ita.models.LanguageSwitcher.UA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomePageTest extends TestRunner {
 
     @Test
-    public void verifyLanguageIsSwitchingTest() {
-        homePage.switchLanguageTo(UA);
-        var isLanguageSwitched = homePage.isLanguageSwitchedTo(UA);
+    public void verifyLanguageSwitchingTest() {
+        var languageToSwitch = homePage.isLanguageSwitchedTo(UA) ? RU : UA;
+        homePage.switchLanguageTo(languageToSwitch);
+        var isLanguageSwitched = homePage.isLanguageSwitchedTo(languageToSwitch);
+
         assertThat(isLanguageSwitched)
                 .as("Language should be switched")
-                .isEqualTo(true);
+                .isTrue();
     }
 }
