d480e4616cc218e324288fa899f280af03062df1
Dmytro Krutenko <dospecwork@gmail.com>
2021-11-25 18:21:20 +0200

    Update SearchResultPage.java

--- src/main/java/com/softserveinc/ita/pageobjects/SearchResultPage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/SearchResultPage.java
@@ -1,10 +1,10 @@
 package com.softserveinc.ita.pageobjects;
 
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.SelenideElement;
 
 import java.util.List;
 
+import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Selenide.$$x;
 import static java.time.Duration.ofSeconds;
 import static java.util.stream.Collectors.toList;
@@ -13,7 +13,7 @@ public class SearchResultPage extends BasePage<SearchResultPage> {
 
     public List<String> getGoodsListBy(String productName) {
         return $$x("//*[@class='goods-tile__title']")
-                .shouldBe(CollectionCondition.sizeGreaterThan(0), ofSeconds(8))
+                .shouldBe(sizeGreaterThan(0), ofSeconds(8))
                 .stream()
                 .map(SelenideElement::getText)
                 .filter(text -> text.contains(productName))
