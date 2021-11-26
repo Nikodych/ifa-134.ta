afc5c8bf933e2c98d0b09c35baae0f1b3028e53a
Dmytro Krutenko <dospecwork@gmail.com>
2021-11-26 11:44:14 +0200

    Update ProductPage.java

--- src/main/java/com/softserveinc/ita/pageobjects/ProductPage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/ProductPage.java
@@ -3,7 +3,7 @@ package com.softserveinc.ita.pageobjects;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 import static java.time.Duration.ofSeconds;
-
+//TODO: move methods not related to this page to other page objects
 public class ProductPage extends BasePage<ProductPage> {
 
     public String getPriceFromFirstItem() {
