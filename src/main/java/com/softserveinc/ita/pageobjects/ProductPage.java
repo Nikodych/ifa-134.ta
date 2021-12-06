eb2927332d26000a2532f38585c53a79bfc0cd7f
Pavlo Kuravskyi <pkuravsky@gmail.com>
2021-12-06 19:21:27 +0200

    Added test to switch product tabs

--- src/main/java/com/softserveinc/ita/pageobjects/ProductPage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/ProductPage.java
@@ -4,11 +4,15 @@ package com.softserveinc.ita.pageobjects;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.String.format;
 import static java.time.Duration.ofSeconds;
 
 //TODO: move methods not related to this page to other page objects
 public class ProductPage extends BasePage<ProductPage> {
+
+    private final String productTabTemplate = "//li[contains(@Class,'tabs__item')]/a[contains(text(),'%s')]";
 
     public String getPriceFromFirstItem() {
         return $x("//ul[@class='catalog-grid ng-star-inserted']/li[1]//span[@class='goods-tile__price-value']")
@@ -32,6 +36,32 @@ public class ProductPage extends BasePage<ProductPage> {
 
     public String getProductCategory() {
         return $x("//ul[@class='breadcrumbs ng-star-inserted']").getText();
+    }
+
+    public ProductPage switchProductTabTo(String productTab) {
+        $x(format(productTabTemplate, productTab)).click();
+
+        return this;
+    }
+
+    public String getActiveTabText() {
+        return $("a.tabs__link--active")
+                .getText()
+                .replaceAll("\\d", "");
+    }
+
+    public boolean isCorrectTabDisplayed() {
+        var activeTabText = getActiveTabText();
+        var productTabHeadingText = $("*.product-tabs__heading").getText();
+
+        switch (activeTabText) {
+            case "Купують разом":
+                return productTabHeadingText.startsWith("Аксесуари");
+            case "Залишити відгук":
+                return productTabHeadingText.startsWith("Додати відгук");
+            default:
+                return productTabHeadingText.contains(activeTabText);
+        }
     }
 }
 
