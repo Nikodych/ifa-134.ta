a70f35f0eb6608e9742e7c53fbb9832379a5a082
Max Makoviichuk <mikimak237@gmail.com>
2021-12-13 17:29:12 +0200

    Added annotation

--- src/main/java/com/softserveinc/ita/utils/NumberUtil.java
+++ src/main/java/com/softserveinc/ita/utils/NumberUtil.java
@@ -1,7 +1,10 @@
 package com.softserveinc.ita.utils;
 
+import lombok.experimental.UtilityClass;
+
 import static java.lang.Integer.parseInt;
 
+@UtilityClass
 public class NumberUtil {
 
     public static int parseIntPrice(String testPrice) {
