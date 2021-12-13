9c26c16d32b490765abeb769ceb2dba764ce396f
DmytroShkliar <dmytro.shkliar@sunpowercorp.com>
2021-12-13 18:39:30 +0200

    Merge_fix

--- src/main/java/com/softserveinc/ita/pageobjects/ProductPage.java
+++ src/main/java/com/softserveinc/ita/pageobjects/ProductPage.java
@@ -4,12 +4,16 @@ package com.softserveinc.ita.pageobjects;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.softserveinc.ita.utils.NumberUtil.parseIntPrice;
+import static java.lang.String.format;
 import static java.time.Duration.ofSeconds;
 
 //TODO: move methods not related to this page to other page objects
 public class ProductPage extends BasePage<ProductPage> {
+
+    private final String PRODUCT_TAB_SELECTOR_TEMPLATE = "//li[contains(@Class,'tabs__item')]/a[contains(text(),'%s')]";
 
     public String getPriceFromFirstItem() {
         return $x("//ul[@class='catalog-grid ng-star-inserted']/li[1]//span[@class='goods-tile__price-value']")
@@ -41,6 +45,33 @@ public class ProductPage extends BasePage<ProductPage> {
 
     public String getProductCategory() {
         return $x("//ul[@class='breadcrumbs ng-star-inserted']").getText();
+    }
+
+    @Step("ProductPage: Switched product tab to '{productTab}'")
+    public ProductPage switchProductTabTo(String productTab) {
+        $x(format(PRODUCT_TAB_SELECTOR_TEMPLATE, productTab)).click();
+
+        return this;
+    }
+
+    public boolean isCorrectTabDisplayed() {
+        var activeTabText = getActiveTabText();
+        var productTabHeadingText = $("*.product-tabs__heading").getText();
+
+        switch (activeTabText) {
+            case "Купують разом":
+                return productTabHeadingText.startsWith("Аксесуари");
+            case "Залишити відгук":
+                return productTabHeadingText.startsWith("Додати відгук");
+            default:
+                return productTabHeadingText.contains(activeTabText);
+        }
+    }
+
+    private String getActiveTabText() {
+        return $("a.tabs__link--active")
+                .getText()
+                .replaceAll("\\d", "");
     }
 }
 
