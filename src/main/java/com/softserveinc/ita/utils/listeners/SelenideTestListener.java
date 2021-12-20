63d0f75072ce1bcae8da2d72c2fb1b4ef50e8061
Dmytro Krutenko <dospecwork@gmail.com>
2021-12-20 12:34:48 +0200

    update

--- src/main/java/com/softserveinc/ita/utils/listeners/SelenideTestListener.java
+++ src/main/java/com/softserveinc/ita/utils/listeners/SelenideTestListener.java
@@ -1,15 +1,18 @@
 package com.softserveinc.ita.utils.listeners;
 
+import com.codeborne.selenide.logevents.LogEvent;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.ITestListener;
 
-import static com.codeborne.selenide.logevents.SelenideLogger.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
-public class SelenideTestListener {
+public class SelenideTestListener implements ITestListener {
 
     static void setupAllureReports() {
         addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(false)
                 .savePageSource(true)
-        );
+                .afterEvent());
     }
 }
--- src/main/java/com/softserveinc/ita/utils/runners/TestRunner.java
+++ src/main/java/com/softserveinc/ita/utils/runners/TestRunner.java
@@ -1,13 +1,13 @@
 package com.softserveinc.ita.utils.runners;
 
-import com.softserveinc.ita.utils.listeners.TestListener;
+import com.softserveinc.ita.utils.listeners.SelenideTestListener;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 
 import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selenide.open;
 
-@Listeners({TestListener.class})
+@Listeners({SelenideTestListener.class})
 public abstract class TestRunner {
 
     @BeforeMethod
